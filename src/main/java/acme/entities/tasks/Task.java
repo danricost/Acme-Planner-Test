f7daf96181929f272a6953d0490a3410d535a535
Daniel Rico Ostos <danricost@alum.us.es>
2021-05-20 12:13:19 +0200

    Initial Commit

---
+++ src/main/java/acme/entities/tasks/Task.java
@@ -0,0 +1,54 @@
+package acme.entities.tasks;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Task extends DomainEntity{
+	
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(min = 1, max = 80)
+	protected String 			title;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date 				initialMoment;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date 				finalMoment;
+	
+	@NotNull
+	protected Double 			workload;
+	
+	@NotBlank
+	@Length(min = 1, max = 500)
+	protected String 			description;
+	
+	@NotNull
+	protected Boolean 			isPublic;
+	
+	// Derived attributes -----------------------------------------------------
+	
+	// Relationships ----------------------------------------------------------
+	
+}
